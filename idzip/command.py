2958102777932197a3d32faa428bc9cf826c50ae
ivo <ivo@danihelka.net>
2010-11-13 17:50:33 +0100

    Added the idzip module on the path.

--- idzip/command.py
+++ idzip/command.py
@@ -6,7 +6,9 @@ Compresses the given file.
 import os
 import sys
 
-import compressor
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, parent_dir)
+from idzip import compressor
 
 SUFFIX = ".dz"
 
