bb9d7a99c2e9d3d484cdc99664807d8697b2272d
ivo <ivo@danihelka.net>
2011-01-04 17:42:31 +0100

    Removed a trailing space.

--- test/test_compressor.py
+++ test/test_compressor.py
@@ -26,6 +26,26 @@ def test_compress_chunks():
     _eq_compress("medium.txt")
 
 
+def test_compress_multiple_members():
+    mtime = 1234
+    first_size = _inputsize(open("test/data/medium.txt", "rb"))
+    second_size = _inputsize(open("test/data/small.txt", "rb"))
+    assert first_size >= second_size
+
+    orig = compressor.MAX_MEMBER_SIZE
+    try:
+        compressor.MAX_MEMBER_SIZE = first_size
+        produced = _mem_compress("two_members.txt", mtime)
+    finally:
+        compressor.MAX_MEMBER_SIZE = orig
+
+    expected = open("test/data/two_members.txt.dz", "rb")
+    _eq_zformat(expected, produced, mtime, expected_basename="two_members.txt",
+            in_size=first_size)
+    _eq_zformat(expected, produced, mtime=0, expected_basename=None,
+            in_size=second_size)
+
+
 def test_big_file():
     header = StringIO()
     in_size = compressor.MAX_MEMBER_SIZE
@@ -41,18 +61,44 @@ def test_big_file():
 
 
 def _eq_compress(basename, mtime=0):
+    output = _mem_compress(basename, mtime)
+    expected = open("test/data/%s.dz" % basename, "rb")
+    _eq_zformat(expected, output, mtime, expected_basename=basename)
+
+
+def _mem_compress(basename, mtime=0):
     output = StringIO()
     with open("test/data/%s" % basename, "rb") as input:
-        input.seek(0, os.SEEK_END)
-        in_size = input.tell()
-        input.seek(0)
-
+        in_size = _inputsize(input)
         compressor.compress(input, in_size, output, basename, mtime)
 
-    
     output.seek(0)
-    expected = open("test/data/%s.dz" % basename, "rb")
-    asserting.eq_bytes(expected.read(4), output.read(4))
+    return output
+
+
+def _inputsize(input):
+    input.seek(0, os.SEEK_END)
+    in_size = input.tell()
+    input.seek(0)
+    return in_size
+
+
+def _eq_zformat(expected, output, mtime=0, expected_basename=None, in_size=None):
+    """Compares the file formats of data in the given streams.
+    """
+    if in_size is None:
+        expected.seek(-4, os.SEEK_END)
+        in_size = struct.unpack("<I", expected.read(4))[0]
+        expected.seek(0)
+
+    # ID1,ID2,CM
+    asserting.eq_bytes(expected.read(3), output.read(3))
+
+    # flags
+    flags = ord(expected.read(1)) & 0xff
+    if expected_basename is None:
+        flags = flags ^ compressor.FNAME
+    eq_(flags, ord(output.read(1)) & 0xff)
 
     # mtime
     eq_(mtime, struct.unpack("<I", output.read(4))[0])
@@ -61,29 +107,41 @@ def _eq_compress(basename, mtime=0):
     # XFL and OS
     asserting.eq_bytes(expected.read(2), output.read(2))
 
-    # field header
+    # FEXTRA header
     info_len = 10
     xlen_bytes = expected.read(2)
     asserting.eq_bytes(xlen_bytes, output.read(2))
+
     xlen = ord(xlen_bytes[0]) & 0xff + 256 * (ord(xlen_bytes[1]) & 0xff)
-    asserting.eq_bytes(expected.read(info_len), output.read(info_len))
-    expected.seek(xlen - info_len, os.SEEK_CUR)
-    output.seek(xlen - info_len, os.SEEK_CUR)
     num_chunks = in_size // compressor.CHUNK_LENGTH
     if in_size % compressor.CHUNK_LENGTH:
         num_chunks += 1
     eq_(num_chunks, (xlen - info_len) // 2)
 
-    # filename
-    fname_len = len(basename) + 1
-    asserting.eq_bytes(expected.read(fname_len), output.read(fname_len))
+    asserting.eq_bytes(expected.read(info_len), output.read(info_len))
+    expected.seek(xlen - info_len, os.SEEK_CUR)
+    output.seek(xlen - info_len, os.SEEK_CUR)
 
+    # FNAME
+    fname = _read_cstring(expected)
+    if expected_basename is not None:
+        eq_(expected_basename, _read_cstring(output))
+
+    # zstream
     _eq_zstream(expected, output)
 
     # tail
-    expected.seek(-8, os.SEEK_END)
-    output.seek(-8, os.SEEK_END)
     asserting.eq_bytes(expected.read(8), output.read(8))
+
+
+def _read_cstring(input):
+    text = ""
+    while True:
+        c = input.read(1)
+        if c == "\0":
+            return text
+
+        text += c
 
 
 def _eq_zstream(expected, produced):
@@ -95,8 +153,10 @@ def _eq_zstream(expected, produced):
     import zlib
     deobj = zlib.decompressobj(-zlib.MAX_WBITS)
     expected_data = deobj.decompress(expected.read())
+    expected.seek(-len(deobj.unused_data), os.SEEK_CUR)
 
     deobj = zlib.decompressobj(-zlib.MAX_WBITS)
     got = deobj.decompress(produced.read())
+    produced.seek(-len(deobj.unused_data), os.SEEK_CUR)
     asserting.eq_bytes(expected_data, got)
 
